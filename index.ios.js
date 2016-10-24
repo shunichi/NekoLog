ab66ca07c2cdf9ad698bc14b4dbb0e2815290514
Shunichi Ikegami <sike.tm@gmail.com>
2016-10-24 10:26:55 +0900

    サーバーからJSONをとってきて表示まで

--- index.ios.js
+++ index.ios.js
@@ -9,25 +9,64 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View
 } from 'react-native';
+import base64 from 'base-64';
+
+const catImages = [
+  require('./images/wancha.jpg'),
+  require('./images/nori.jpg'),
+];
+
+class Cat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {id: this.props.id, name: this.props.name};
+  }
+
+  render() {
+    let cat = this.state
+    return (
+      <View>
+        <Image source={catImages[cat.id % catImages.length]} style={{width: 120, height: 120}} />
+        <Text>{cat.name}</Text>
+      </View>
+    );
+  }
+}
 
 export default class NekoLog extends Component {
+  constructor(props){
+    super(props);
+    this.state = {text: '', cats: []};
+  }
+
+  componentDidMount() {
+    this.fetchCats();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        {this.state.cats.map((cat,_) =>
+          <Cat key={cat.id} id={cat.id} name={cat.name} />
+        )}
       </View>
     );
+  }
+
+  fetchCats() {
+    let headers = new Headers();
+    headers.append("Authorization", "Basic " + base64.encode("user:pass"));
+    fetch('https://neko-log.herokuapp.com/cats.json', {headers: headers})
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({cats: responseJson});
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }
 }
 
